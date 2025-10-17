5e2a55f919aa4d7f2d02365f86da65f91bc80874
Alexsandr <111351345+AlexsandrZolotarev@users.noreply.github.com>
2025-10-17 15:12:27 +0300

    feat(Footer): add markup and styles

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,31 +1,33 @@
 import Breadcrumbs from './components/Breadcrumbs/Breadcrumbs';
+import Footer from './components/Footer/Footer';
 import Toolbar from './components/Toolbar/Toolbar';
 import './styles/main.scss';
 function App() {
   return (
-    <main>
+    <>
       <header className="header container">
         <Breadcrumbs />
       </header>
-      <section aria-labelledby="users" className="users">
-        <div className="users__main">
-          <div className="users__body">
-            <div className="users__header container">
-              <h1 className="users__title" id="users">
-                Пользователи
-              </h1>
-              <Toolbar />
-            </div>
-            <div className="hero__body-inner">
-              {/* <Table />
+      <main>
+        <section aria-labelledby="users" className="users">
+          <div className="users__main">
+            <div className="users__body">
+              <div className="users__header container">
+                <h1 className="users__title" id="users">
+                  Пользователи
+                </h1>
+                <Toolbar />
+              </div>
+              <div className="hero__body-inner">
+                {/* <Table />
               <Pagination /> */}
+              </div>
             </div>
           </div>
-        </div>
-      </section>
-
-      {/* <Footer/> */}
-    </main>
+        </section>
+      </main>
+      <Footer />
+    </>
   );
 }
 
--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -1,5 +1,44 @@
 function Footer() {
-  return <footer>Footer</footer>;
+  return (
+    <footer className="footer">
+      <div className="footer__inner container">
+        <nav className="footer__body" aria-label="Ссылки футера">
+          <ul className="footer__list">
+            <li className="footer__item">
+              <a href="#">Информация</a>
+            </li>
+            <li className="footer__item">
+              <a href="#">Пользователи</a>
+            </li>
+            <li className="footer__item">
+              <a href="#">Реклама</a>
+            </li>
+            <li className="footer__item">
+              <a href="#">Условия</a>
+            </li>
+            <li className="footer__item">
+              <a href="#">Политика Конфиденциальности</a>
+            </li>
+            <li className="footer__item">
+              <a href="#">Поддержка</a>
+            </li>
+            <li className="footer__item">
+              <a href="#">Клиенты</a>
+            </li>
+            <li className="footer__item">
+              <a href="#">Настройки Куки</a>
+            </li>
+            <li className="footer__item">
+              <a href="#">Главная</a>
+            </li>
+          </ul>
+        </nav>
+        <div className="footer__extra">
+          <div className="footer__copyright">@test</div>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
